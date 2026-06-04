021504221920ce3bac9b41c8450c4dd0f22a5ebb
actionchain <actionchain@chainguard.dev>
2026-06-04 16:02:10 +0000

    Update crate-ci/typos@v1.46.3

---
+++ setup.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+
+from setuptools import setup
+
+
+TYPOS_VERSION = '1.46.3'
+
+
+setup(
+    name='pre_commit_placeholder_package',
+    version='0.0.0',
+    install_requires=[f'typos=={TYPOS_VERSION}'],
+    package_dir={'': 'crates'},
+)
